20d8a67b12e3cc86083ec7af9c8c523178a2fe80
MohammedMahdi <99882304+MoeMahdi21@users.noreply.github.com>
2025-09-15 19:55:30 -0400

    add logic for the grid size button to take size from user and make the squers size flexable based on user input and finaly add rgb background

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
 const container = document.querySelector('#grid-container');
+const btn = document.querySelector('#grid-btn');
+btn.addEventListener('click', () => {
+  let userInput = prompt('Please Enter grid size');
+  if (userInput <= 100) {
+    createGrid(userInput);
+  } else {
+    alert('Grid Size max limit is 100');
+  }
+});
 
-for (let i = 1; i <= 256; i++) {
-  const newDiv = document.createElement('div');
-  newDiv.addEventListener('mouseenter', () => {
-    newDiv.style.background = 'black';
-  });
-  container.appendChild(newDiv);
+let rgbBackground = function () {
+  return Math.floor(Math.random() * 256);
+};
+
+function createGrid(size = 0) {
+  container.textContent = '';
+  let pixelsWide = 960 / size;
+  for (let i = 1; i <= size * size; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.style.width = pixelsWide + 'px';
+    newDiv.style.height = pixelsWide + 'px';
+
+    newDiv.addEventListener('mouseenter', () => {
+      newDiv.style.background = `rgb(${rgbBackground()}, ${rgbBackground()}, ${rgbBackground()})`;
+    });
+    container.appendChild(newDiv);
+  }
 }
