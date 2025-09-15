6015c41d8054e1286436446a7d5c5dcb82860366
MohammedMahdi <99882304+MoeMahdi21@users.noreply.github.com>
2025-09-15 18:49:16 -0400

    add event listener

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ const container = document.querySelector('#grid-container');
 
 for (let i = 1; i <= 256; i++) {
   const newDiv = document.createElement('div');
+  newDiv.addEventListener('mouseenter', () => {
+    newDiv.style.background = 'black';
+  });
   container.appendChild(newDiv);
 }
